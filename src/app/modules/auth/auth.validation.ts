fb2150593e1002affc9522c0e20b0b67fe35e478
FajlaRabby24 <fajlarabbyla24@gmail.com>
2026-03-28 02:44:03 +0600

    feat: simplify registration schema by removing unused fields

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -1,5 +1,4 @@
 import { z } from "zod";
-import { Role } from "../../../generated/prisma/enums";
 
 export const registerSchema = z.object({
   name: z
@@ -8,29 +7,16 @@ export const registerSchema = z.object({
     .max(50, "Name must be at most 50 characters long"),
 
   email: z.email("Email must be a valid email address"),
-
-  emailVerified: z.boolean().default(false),
-
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .max(50, "Password must be at most 50 characters long"),
   image: z.url("Image must be a valid URL").nullable().optional(),
-
-  role: z.enum([Role.USER, Role.ADMIN]).default(Role.USER).optional(),
-
   bio: z
     .string()
     .max(250, "Bio must be at most 250 characters long")
     .nullable()
     .optional(),
-
-  isActive: z.boolean().default(true).optional(),
-
-  isBanned: z.boolean().default(false).optional(),
-
-  bannedReason: z.string().nullable().optional(),
-
-  bannedAt: z.date().nullable().optional(),
-
-  createdAt: z.date().optional(),
-  updatedAt: z.date().optional(),
 });
 
 export const authValidation = {
